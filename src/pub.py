944021c37b2369715a449e08901d15c3546fdb20
NTUMBABU <tshiteyababci@gmail.com>
2021-02-23 17:18:37 +0400

    ajout du fichier pub

--- src/pub.py
+++ src/pub.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
- # license removed for brevity
 import rospy
-from std_msgs.msg import String
- 
-def talker():
-    pub = rospy.Publisher('published_topic', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(15) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        #rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+from geometry_msgs.mgs import PoseStamped
+import math
 
-if __name__ == '__main__':
+def talker():
+     pub = rospy.Publisher('chatter',PoseStamped,queue,queue_size=10)
+     rospy.init_node('talker',anonymous=True)
+     rate = rospy.Rate(15)
+     message.pose.position.x = 22
+     print(message)
+     while not rospy.is_shutdown():
+         message.header.frame_id = "map"
+         r = 10	
+         theta = 0
+
+         while theta < 2*(math.pi):
+             message.pose.position.x = theta
+             message.pose.position.y = math.sin(theta)
+             print(message)
+           #rospy.loginfo(hello_str)
+             pub.publish(message)
+             rate.sleep()
+             theta = theta + 0.1
+
+         while theta >= 0:
+           message.pose.position.x = theta
+           message.pose.position.y = math.sin(-theta)
+           print(message)
+           #rospy.loginfo(hello_str)
+           pub.publish(message)
+           rate.sleep()
+           theta = theta - 0.1
+           print(message)
+
+if name == 'main': 
     try:
         talker()
-    except rospy.ROSInterruptException:
+    except rospy.ROSInterruptException: 
         pass
